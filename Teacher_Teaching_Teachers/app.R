f18d4bf349a79f2900cb2ee19dde54c0a71a5110
alyssapeer <apeer@luc.edu>
2025-04-25 16:50:59 -0500

    Updates to app

--- Teacher_Teaching_Teachers/app.R
+++ Teacher_Teaching_Teachers/app.R
@@ -1,42 +1,71 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# Authors: Mena & Alyssa
+# Last Updated: 4/25/2025
+# Intended Use: Create a dashboard for students to upload and visualize 
+# PurpleAir data
+############################################################################
 
+# Load packages
 library(shiny)
 library(tidyverse)
+library(shinydashboard)
+library(sf)
+library(lubridate)
+library(bslib)
 
+# Load and clean data
 dat <- read_csv("4-11dat.csv", skip = 8)
-### how to clean this
-## think of other things
 names(dat) <- c("id", "session", "date", "longitude", "latitude", "temp", "pm1", "pm10", "pm2_5", "humidity")
 dat <- dat %>% 
   mutate(date = ymd_hms(date))
 
+## Define theme for dashboard
+# Create a custom theme
+my_theme <- bs_theme(bg = "#F7F7F7",
+                     fg = "#AA44AA",
+                     accent = "#A7F7BE",
+                     primary = "#AA44AA",
+                     base_font = "sans-serif",
+                     bootswatch = "cosmo"
+                     )
+#bs_theme_preview(my_theme)
+
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-    ## slider for picking time aggregation
+ui <- page_navbar(
+  theme = my_theme,
+  title = "CARES Student Dashboard",
+  
+  nav_panel(
+    "Home",
+    h2("Welcome to the Dashboard"),
+    ## Temporal Aggregation Value (Seconds)
     sliderInput(
-        "time_agg",
-        "Set Temporal Aggregation Value (Seconds)",
-        min = 1,
-        max = 60,
-        value = 3,
-        step = 1
-      ),
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+      "time_agg",
+      "Set Temporal Aggregation Value (Seconds)",
+      min = 1,
+      max = 60,
+      value = 3,
+      step = 1
+    ),
+    plotOutput("distPlot")
+  ),
+  
+  nav_panel(
+    "Upload",
+    h3("Upload Sensor Data"),
+    p("This page will be used to upload data.")
+  ),
+
+  nav_panel(
+    "About",
+    h3("About this Dashboard"),
+    p("This is a student dashboard using a custom Bootstrap theme!")
+  )
+
+ )
 
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
   
     time_averaged <- reactive({
     
