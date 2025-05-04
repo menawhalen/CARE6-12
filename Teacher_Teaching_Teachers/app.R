e9670328f4f20d7f3f9c00622624b26564eaecf1
alyssapeer <apeer@luc.edu>
2025-05-04 12:56:36 -0500

    little update

--- Teacher_Teaching_Teachers/app.R
+++ Teacher_Teaching_Teachers/app.R
@@ -7,7 +7,6 @@
 # Load packages
 library(shiny)
 library(tidyverse)
-library(shinydashboard)
 library(sf)
 library(lubridate)
 library(bslib)
@@ -20,32 +19,53 @@ dat <- dat %>%
 
 ## Define theme for dashboard
 # Create a custom theme
-my_theme <- bs_theme(bg = "#F7F7F7",
+my_theme <- bs_theme(
+                    bg = "#F7F7F7",
                      fg = "#AA44AA",
                      accent = "#A7F7BE",
                      primary = "#AA44AA",
                      base_font = "sans-serif",
-                     bootswatch = "cosmo"
+                     bootswatch = "yeti",
+                     rounded = TRUE,
+              "card-border-radius" = "1rem"       # specifically for cards and value_box()
                      )
 #bs_theme_preview(my_theme)
 
 # Define UI for application that draws a histogram
 ui <- page_navbar(
   theme = my_theme,
-  title = "CARES Student Dashboard",
+  title = "CARE Teacher Dashboard",
+  nav_spacer(), # push nav items to the right
   
   nav_panel(
     "Home",
     h2("Welcome to the Dashboard"),
-    ## Temporal Aggregation Value (Seconds)
-    sliderInput(
-      "time_agg",
-      "Set Temporal Aggregation Value (Seconds)",
-      min = 1,
-      max = 60,
-      value = 3,
-      step = 1
+    layout_columns(
+      bslib::value_box(
+        title = "Total Number of Sensors", value = n_distinct(dat$session), theme = "info",
+        showcase = fontawesome::fa_i("hashtag"), showcase_layout = "top right",
+        full_screen = FALSE, fill = TRUE, height = NULL,
+        card_wrapper =TRUE
+      ),
+      bslib::value_box(
+        title = "Average PM2.5 Levels", value = round(mean(dat$pm2_5,na.rm = TRUE), digits = 2),
+        theme = "secondary", showcase = fontawesome::fa_i("smog"),
+        showcase_layout = "top right", full_screen = FALSE, fill = TRUE,
+        height = NULL,
+        card_wrapper =TRUE
+      ),
+      bslib::value_box(
+        title = "Standard Deviation of PM2.5 Levels", value = round(sd(dat$pm2_5,na.rm = TRUE), digits = 2),
+        theme = "primary", showcase = fontawesome::fa_i("hand-holding-dollar"),
+        showcase_layout = "top right", full_screen = FALSE, fill = TRUE,
+        height = NULL,
+        card_wrapper =TRUE
+      )
     ),
+    ## Temporal Aggregation Value (Seconds)
+    radioButtons("time_agg", label = h3("Select Temporal Aggregation Value (s)"),
+                 choices = list("10s" = 10, "30s" = 20, "60s" = 60), 
+                 selected = 3),
     plotOutput("distPlot")
   ),
   
