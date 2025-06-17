d0df9a99e7a4678f2f69caafb2d9d4446c9a38c9
Mena Whalen <menawhalen@gmail.com>
2025-06-17 12:01:35 -0500

    updating database

--- database_creation.R
+++ database_creation.R
@@ -9,41 +9,45 @@ library(readr)
 
 library(dplyr)
 library(lubridate)
+library(purrr)
+library(stringr)
 
-
-con <- dbConnect(RSQLite::SQLite(), "C:/Users/noral/OneDrive/Desktop/CDSC/care6-12/CARE6-12/care612Database.db")
+# con <- dbConnect(RSQLite::SQLite(), "C:/Users/noral/OneDrive/Desktop/CDSC/care6-12/CARE6-12/care612Database.db")
 
 # dat1 <- read_csv("data/1.csv", skip = 8)
 # names(dat1) <- c("id", "session", "date", "latitude","longitude", "temp", "pm1", "pm10", "pm2_5", "humidity")
 # dat1 <- dat1 %>% mutate(date = ymd_hms(date))
 
+con <- dbConnect(RSQLite::SQLite(), "/Users/menawhalen/Library/CloudStorage/OneDrive-LoyolaUniversityChicago/Consulting/care_6_12/careDB.db")
 
 
 clean_data <- function(file_path) {
   df <- read_csv(file_path, skip = 8)
   
-  names(df) <- c("id", "session", "date", "latitude", "longitude", "temp", "pm1", "pm10", "pm2_5", "humidity")
+  names(df) <- c("row_id", "session", "date", "latitude", "longitude", "temp", "pm1", "pm10", "pm2_5", "humidity")
   
   df <- df %>%
-    mutate(date = ymd_hms(date))
+    mutate(date = ymd_hms(date),
+           session_id = paste0(str_to_lower(str_replace_all(session, " ", "_")), "_", min(date))) %>% 
+    select(session_id, session, date, latitude, longitude, temp, pm1, pm10, pm2_5, humidity)
   
   return(df)
 }
 
-csv_files <- list.files("C:/Users/noral/OneDrive/Desktop/CDSC/care6-12/CARE6-12/data", pattern = "\\.csv$", full.names = TRUE)
-
-# Loop through files, clean and write each to the database
-for (file in csv_files) {
-  table_name <- tools::file_path_sans_ext(basename(file))
-  
-  df_clean <- clean_data(file)
-  
-  dbWriteTable(con, table_name, df_clean, overwrite = TRUE)
-  
-  cat("Imported cleaned table:", table_name, "\n")
-}
+csv_files <- list.files("/Users/menawhalen/Library/CloudStorage/OneDrive-LoyolaUniversityChicago/Consulting/care_6_12/data", pattern = "\\.csv$", full.names = TRUE)
 
 
+dat <- map(csv_files, ~ clean_data(.x))
+
+# map(dat, ~.x %>% pull(session) %>% unique())
+
+dat <- dat %>% 
+  bind_rows() %>% 
+  mutate(session = factor(session))
+
+unique(dat$session_id)
+
+dbWriteTable(conn = con, name = "historical", dat)
 
 dbListTables(con)
 # Disconnect when done
